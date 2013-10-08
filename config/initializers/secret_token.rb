dfa10f9bedb60f6f53b285462971c4a917dda01e
Richard Davis <rdavis.bacs@gmail.com>
2013-10-07 21:04:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Matchmaker::Application.config.secret_key_base = '86b47734ff3dc3180c454459237b8f6cd3c8fc3fdd10a18f3b74991cfa2ebfc36045028854217ae019b13f84d8385cd584801b81555a4d1a19ba1d6ad26b09cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Matchmaker::Application.config.session_store :cookie_store, key: '_matchmaker_session'
