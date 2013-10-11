2698d336aa0192c0d3f1279bdaed98ffbdeb1755
Adam Sheehan <a.t.sheehan@gmail.com>
2013-10-10 21:05:57 -0400

    Refactor company skills form and test.
    Allow the skill_ids to be set via the form params rather than
    explicitly setting them in the controller. Required changes
    to the strong parameters to allow the skill_ids to pass through.

--- spec/features/create_company_profile_spec.rb
+++ spec/features/create_company_profile_spec.rb
@@ -13,36 +13,63 @@ feature 'create company profile', %q{
   #   telecommuting(boolean), industry(list of options),
   #   company size,  year founded
 
-  scenario 'user creates a company profile completely' do
-    previous_count = Company.count
+  context 'as an authenticated user' do
 
-    user = FactoryGirl.create(:user)
-    industry = FactoryGirl.create(:industry)
-    sign_in_as(user)
+    let(:user) { FactoryGirl.create(:user) }
 
-    click_on 'Add a Company'
-    fill_in 'Name', with: 'Cyberdine Systems'
-    fill_in 'Email', with: 'hr@cyberdine.com'
-    fill_in 'City', with: 'Los Angeles'
-    fill_in 'State', with: 'CA'
-    fill_in 'Description', with: 'Some California company'
-    check 'Telecommute'
-    select industry.name, from: 'Industry'
-    select 'Startup (1-25)', from: 'company_size'  # startup, mid-sized, international conglomerate
-    fill_in 'Year founded', with: 2013
-    click_on 'Submit'
+    before :each do
+      FactoryGirl.create(:industry, name: 'Shark Wrangling')
+      sign_in_as(user)
+    end
 
-    expect(page).to have_content('Your company information was recorded')
-    expect(Company.count).to eql(previous_count + 1)
-  end
+    scenario 'create a company profile' do
+      previous_count = Company.count
 
-  scenario 'user tries to create a company without required fields' do
-    previous_count = Company.count
-    user = FactoryGirl.create(:user)
-    sign_in_as(user)
-    click_on 'Add a Company'
-    click_on 'Submit'
-    expect(page).to have_content("can't be blank")
-    expect(Company.count).to eql(previous_count)
+      visit new_company_path
+      fill_in_company_details
+      click_on 'Submit'
+
+      expect(page).to have_content('Your company information was recorded')
+      expect(Company.count).to eql(previous_count + 1)
+    end
+
+    scenario 'require all fields for profile' do
+      previous_count = Company.count
+
+      visit new_company_path
+      click_on 'Submit'
+
+      expect(page).to have_content("can't be blank")
+      expect(Company.count).to eql(previous_count)
+    end
+
+    scenario 'select multiple skills needed' do
+      javascript = FactoryGirl.create(:skill, name: 'Javascript')
+      cpp = FactoryGirl.create(:skill, name: 'C++')
+      ruby = FactoryGirl.create(:skill, name: 'Ruby')
+
+      visit new_company_path
+      fill_in_company_details
+
+      check 'Ruby'
+      check 'Javascript'
+      click_on 'Submit'
+
+      company = Company.last
+      expect(company.skills).to include(ruby, javascript)
+      expect(company.skills).to_not include(cpp)
+    end
+
+    def fill_in_company_details
+      fill_in 'Name', with: 'Cyberdine Systems'
+      fill_in 'Email', with: 'hr@cyberdine.com'
+      fill_in 'City', with: 'Los Angeles'
+      fill_in 'State', with: 'CA'
+      fill_in 'Description', with: 'Some California company'
+      check 'Telecommute'
+      select 'Shark Wrangling', from: 'Industry'
+      select 'Startup (1-25)', from: 'Company Size'
+      fill_in 'Year founded', with: 2013
+    end
   end
 end
