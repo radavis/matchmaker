54e648c7be7393b288f115214294e156e6b734c1
Richard Davis <rdavis.bacs@gmail.com>
2013-10-11 16:18:38 -0400

    seeding company data is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Seeders::Industries.seed
-#Seeders::Companies.seed
+Seeders::Companies.seed
--- lib/seeders/companies.rb
+++ lib/seeders/companies.rb
@@ -10,39 +10,42 @@ module Seeders
       def companies
         results = []
         CSV.foreach("#{Rails.root}/db/data/companies.csv",
-        #CSV.foreach("db/data/companies.csv",
           headers: true,
           header_converters: :symbol) do |row|
           results << row.to_hash
         end
-        puts 'normalize'
         normalize(results)
       end
 
       def normalize(records)
         columns = [ :name, :email, :city, :state, :description, :telecommute, :size, :year_founded, :industry_id ]
 
-        puts 'enter normalize'
+        #puts 'enter normalize'
         records.each do |record|
 
-          if record[:industry]
-            industry = Industry.find_by(name: record[:industry])
+          # if record[:industry]
+          #   industry = Industry.find_by(name: record[:industry])
 
-            if !industry.nil?
-              record[:industry] = industry.id
-            else
-              industry = Industry.create!(name: record[:industry])
-              record[:industry_id] = industry.id
-            end
+          #   if !industry.nil?
+          #     record[:industry] = industry.id
+          #   else
+          #     industry = Industry.create!(name: record[:industry])
+          #     record[:industry_id] = industry.id
+          #   end
 
-          else
-            record[:industry_id] = Industry.find_by(name: "Computer Software").id
-          end
+          # else
+          #   record[:industry_id] = Industry.find_by(name: "Computer Software").id
+          # end
 
-
-          record[:email] = 'user@company.com' if record[:email].nil?
+          record[:description] = record[:company_info]
+          record[:industry_id] = Industry.find_by(name: "Computer Software").id
+          record[:name] = record[:company]
+          record[:email] = "hr@#{record[:name].parameterize.underscore}.com" if record[:email].nil?
           record[:telecommute] = false if record[:telecommute].nil?
           record[:year_founded] = 2013 if record[:year_founded].nil?
+          record[:description] = "Sorry, no description available." if record[:description].nil?
+          record[:city] = record[:town]
+          record[:size] = "Funded (25-100)"
 
           record.keep_if { |k, v| columns.include?(k) }
 
@@ -56,7 +59,7 @@ module Seeders
           if entry.nil?
             entry = Company.new(company)
           else
-            #entry = company
+            entry.update_attributes(company)
           end
           #binding.pry
           entry.save!
